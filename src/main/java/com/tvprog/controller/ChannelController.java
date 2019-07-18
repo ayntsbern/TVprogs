aa45777706513f88d28ac1777afc1d94bd8dfb88
ayntsbern <ayntsbern@users.noreply.github.com>
2019-07-18 18:29:44 +0300

    add function of deleting tv programme

--- src/main/java/com/tvprog/controller/ChannelController.java
+++ src/main/java/com/tvprog/controller/ChannelController.java
@@ -2,12 +2,14 @@ package com.tvprog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.tvprog.domain.Channel;
 import com.tvprog.service.ChannelService;
 import com.tvprog.service.ChannelServiceImpl;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,8 +23,44 @@ public class ChannelController {
         model.addAttribute("channelList", channels);
         return "channel";
     }
-    @RequestMapping(value = "/programme", method=RequestMethod.GET)
-    public String getProgramme() {
+
+    @RequestMapping(value="/programme/{id}", method=RequestMethod.GET)
+    public String getProgramme(Model model, @PathVariable("id") Integer id) {
+        Channel channels = channelService.getById(id);
+        if (channels == null) {
+            model.addAttribute("msg", "User not found");
+        } else {
+            List<Channel.TVprogramme> tVprogrammeList = channels.getProgramme();
+            model.addAttribute("programmeList", tVprogrammeList);
+            model.addAttribute("id", id);
+        }
         return "programme";
+    }
+    @RequestMapping(value = "/add-new-programme/{id}", method=RequestMethod.GET)
+    public String addNewProgrammePage(Model model, @PathVariable("id") Integer id) {
+        //Channel.TVprogramme tv = new Channel.TVprogramme();
+        //Channel channels = channelService.getById(id);
+        //channels.getProgramme()
+        model.addAttribute("id", id);
+        return "addNewProgramme";
+    }
+
+    @RequestMapping(value = "/add/{id}", method=RequestMethod.GET)
+    public String addNewProgramme(@RequestParam(value="time") String time,
+                                  @RequestParam(value="progTitle") String progTitle,
+                                  @PathVariable("id") Integer id) {
+        Channel channel = channelService.getById(id);
+        channel.setProgramme(time, progTitle);
+        return "redirect:/programme/{id}";
+    }
+
+    @RequestMapping(value="delete/{id}/{time}", method=RequestMethod.GET)
+    public String deleteItem(@PathVariable (value="id") Integer id,
+                             @PathVariable(value="time") String time) {
+        Channel channel = channelService.getById(id);
+        List<Channel.TVprogramme> tVprogrammeList = channel.getProgramme();
+        channelService.delete(tVprogrammeList, time);
+        //orderService.delete(order);
+        return "redirect:/programme/{id}";
     }
 }
