f8ffb177c4a9bcd87fde8ee5fec566e569a00f56
ayntsbern <ayntsbern@users.noreply.github.com>
2019-07-18 18:30:16 +0300

    add function of deleting tv programme

--- src/main/java/com/tvprog/service/ChannelServiceImpl.java
+++ src/main/java/com/tvprog/service/ChannelServiceImpl.java
@@ -4,13 +4,43 @@ import com.tvprog.domain.Channel;
 import com.tvprog.repository.ChannelRepository;
 import com.tvprog.repository.ChannelRepositoryImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ChannelServiceImpl implements ChannelService {
     private ChannelRepository channelRepository = new ChannelRepositoryImpl();
 
+//    public void addProg(Channel.TVprogramme tVprogramme) {
+//        if(tVprogramme!=null) {
+//            List<Channel.TVprogramme> tvprogrammes = channelRepository.getAll();
+//            if(!channels.isEmpty()) {
+//                Channel lastChannel = channels.get(channels.size() - 1);
+//                channel.setId(lastChannel.getId()+1);
+//                channelRepository.add(channel);
+//            }
+//        }
+//    }
+
+//    public void delete(Channel channel) {
+//        if(channel!=null) {
+//            channelRepository.delete(channel);
+//        }
+//    }
+
+//    public  List<Channel.TVprogramme> getAllProg (){
+//        channelRepository.
+//    }
+    public void delete(List<Channel.TVprogramme> tVprogramme, String time){
+        if(time!=null&&tVprogramme!=null){
+            channelRepository.delete(tVprogramme, time);
+        }
+    }
     public List<Channel> getAll(){
         return  channelRepository.getAll();
+    }
+
+    public Channel getById(Integer id) {
+        if(id!=null) return channelRepository.getById(id);
+
+        return null;
     }
 }
