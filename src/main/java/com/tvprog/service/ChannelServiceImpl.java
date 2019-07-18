43c2ca5e318b0eec83b70b45eada0624ceb5827c
ayntsbern <ayntsbern@users.noreply.github.com>
2019-07-18 21:10:53 +0300

    add function of deleting tv programme

--- src/main/java/com/tvprog/service/ChannelServiceImpl.java
+++ src/main/java/com/tvprog/service/ChannelServiceImpl.java
@@ -4,36 +4,28 @@ import com.tvprog.domain.Channel;
 import com.tvprog.repository.ChannelRepository;
 import com.tvprog.repository.ChannelRepositoryImpl;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ChannelServiceImpl implements ChannelService {
     private ChannelRepository channelRepository = new ChannelRepositoryImpl();
 
-//    public void addProg(Channel.TVprogramme tVprogramme) {
-//        if(tVprogramme!=null) {
-//            List<Channel.TVprogramme> tvprogrammes = channelRepository.getAll();
-//            if(!channels.isEmpty()) {
-//                Channel lastChannel = channels.get(channels.size() - 1);
-//                channel.setId(lastChannel.getId()+1);
-//                channelRepository.add(channel);
-//            }
-//        }
-//    }
-
-//    public void delete(Channel channel) {
-//        if(channel!=null) {
-//            channelRepository.delete(channel);
-//        }
-//    }
-
-//    public  List<Channel.TVprogramme> getAllProg (){
-//        channelRepository.
-//    }
     public void delete(List<Channel.TVprogramme> tVprogramme, String time){
         if(time!=null&&tVprogramme!=null){
             channelRepository.delete(tVprogramme, time);
         }
     }
+
+    public void update(Channel.TVprogramme tVprogramme, String time, String progTitle){
+        if(time!=null&&progTitle!=null){
+            channelRepository.update(tVprogramme, time, progTitle);
+        }
+    }
+
+    public void updateList(List<Channel.TVprogramme> tVprogramme){
+        if(tVprogramme!=null) Collections.sort(tVprogramme);
+    }
+
     public List<Channel> getAll(){
         return  channelRepository.getAll();
     }
