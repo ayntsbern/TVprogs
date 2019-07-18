88587cdc51f3e04284c55b1698c9031665ee0dfe
ayntsbern <ayntsbern@users.noreply.github.com>
2019-07-18 21:10:31 +0300

    add function of deleting tv programme

--- src/main/java/com/tvprog/repository/ChannelRepositoryImpl.java
+++ src/main/java/com/tvprog/repository/ChannelRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.tvprog.repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,33 +20,42 @@ public class ChannelRepositoryImpl implements ChannelRepository {
         Channel channel5 = new Channel(5, "Пятый канал");
         Channel channel6 = new Channel(6, "Культура");
 
-        channel1.setProgramme("15:00", "Время с субтитрами");
-        channel1.setProgramme("12:15", "Время покажет");
-        channel1.setProgramme("10:55", "Жить здорово");
+        channel1.setProgramme(0,"15:00", "Время с субтитрами");
+        channel1.setProgramme(1,"12:15", "Время покажет");
+        channel1.setProgramme(2,"10:55", "Жить здорово");
         channels.add(channel1);
-        channel2.setProgramme("14:45", "Кто против?"); //Познавательное
-        channel2.setProgramme("12:50","60 минут");
-        channel2.setProgramme("8:00","Разговорчики");
+        channel2.setProgramme(0,"14:45", "Кто против?"); //Познавательное
+        channel2.setProgramme(1,"12:50","60 минут");
+        channel2.setProgramme(2,"8:00","Разговорчики");
         channels.add(channel2);
-        channel3.setProgramme("14:30", "Водное поло. Чемпионат мира по водным видам спорта. Женщины. " +
+        channel3.setProgramme(0,"14:30", "Водное поло. Чемпионат мира по водным видам спорта. Женщины. " +
                 "Прямая трансляция из Кореи. " +
                 "Россия - Венгрия");
         channels.add(channel3);
-        channel4.setProgramme("14:00", "Ментовские войны 6");
-        channel4.setProgramme("19:00", "Сегодня");
+        channel4.setProgramme(0,"14:00", "Ментовские войны 6");
+        channel4.setProgramme(1,"19:00", "Сегодня");
         channels.add(channel4);
-        channel5.setProgramme("13:25","Береговая охрана. Боевое крещение, 1-я серия");
-        channel5.setProgramme("13:00","Известия");
-        channel5.setProgramme("12:00","Гаишники-2. 10-я серия");
-        channel5.setProgramme("05:25","Страх в твоем доме. Возврату не подлежит");
-        channel5.setProgramme("19:00","След. Сорок свечей");
+        channel5.setProgramme(0,"13:25","Береговая охрана. Боевое крещение, 1-я серия");
+        channel5.setProgramme(1,"13:00","Известия");
+        channel5.setProgramme(2,"12:00","Гаишники-2. 10-я серия");
+        channel5.setProgramme(3,"05:25","Страх в твоем доме. Возврату не подлежит");
+        channel5.setProgramme(4,"19:00","След. Сорок свечей");
         channels.add(channel5);
-        channel6.setProgramme("14:05","Была ли виновна Мария-Антуанетта?");
+        channel6.setProgramme(0,"14:05","Была ли виновна Мария-Антуанетта?");
         channels.add(channel6);
     }
 
-    public void add(Channel order) {
-        channels.add(order);
+    public void update(Channel.TVprogramme tVprogramme, String time, String progTitle){
+        if (tVprogramme.getTime().compareTo(time) != 0) {
+            tVprogramme.setTime(time);
+        }
+        if (tVprogramme.getProgtitle().compareTo(progTitle) != 0) {
+            tVprogramme.setProgtitle(progTitle);
+        }
+        if(tVprogramme.getTime().compareTo(time)!=0 && tVprogramme.getProgtitle().compareTo(progTitle)!=0){
+            tVprogramme.setTime(time);
+            tVprogramme.setProgtitle(progTitle);
+        }
     }
 
     public void delete(List<Channel.TVprogramme> tVprogramme, String time){
