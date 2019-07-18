540555b207fd224cb9520a44ae256a1499718c01
ayntsbern <ayntsbern@users.noreply.github.com>
2019-07-18 22:31:05 +0300

    delete some comments

--- src/main/java/com/tvprog/repository/ChannelRepositoryImpl.java
+++ src/main/java/com/tvprog/repository/ChannelRepositoryImpl.java
@@ -1,16 +1,12 @@
 package com.tvprog.repository;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import com.tvprog.domain.Channel;
-//import com.tvprog.domain.Channel.TVprogramme;
 
 public class ChannelRepositoryImpl implements ChannelRepository {
     private List<Channel> channels = new ArrayList<>();
-    //private List<Channel.TVprogramme> progList = new LinkedList<>();
 
     public ChannelRepositoryImpl(){
         Channel channel1 = new Channel(1, "Первый");
